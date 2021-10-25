13ef4ae8d9de17266610399a16f99a7ac48ec444
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-24 22:41:17 -0500

    stretch challenge: create bank account types & test

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number, account_type, balance):
         self.name = full_name
         self.account = account_number
+        self.type = account_type
         self.balance = balance
 
     def deposit(self, amount):
@@ -25,8 +26,14 @@ class BankAccount:
         return self.balance
 
     def add_interest(self):
-        interest = self.balance * 0.00083
-        self.balance += interest
+        #if it's a savings account, accrue 1% interest per month
+        if self.type == 'savings':
+            interest = self.balance * 0.001
+            self.balance += interest
+        #if it's not a savings account, accrue .083% interest per month
+        else:
+            interest = self.balance * 0.00083
+            self.balance += interest
 
     def print_statement(self):
         #create a string with asterisks to represent hidden digits
@@ -41,7 +48,7 @@ class BankAccount:
 
 ### Test code for each method
 
-brooklyn = BankAccount('Brooklyn', '227658021', 1000.00)
+brooklyn = BankAccount('Brooklyn', '227658021', 'savings', 1000.00)
 brooklyn.deposit(120)
 brooklyn.withdraw(1000)
 brooklyn.get_balance()
@@ -50,7 +57,7 @@ brooklyn.print_statement()
 brooklyn.deposit(341.33)
 
 ### Sample Code
-mitchell = BankAccount('Mitchell Hudson', '3141592', 0)
+mitchell = BankAccount('Mitchell Hudson', '3141592', 'checking', 0)
 #deposit $400,000 into "Mitchell's" account. 
 mitchell.deposit(400000)
 #print a statement
@@ -63,3 +70,12 @@ mitchell.print_statement()
 mitchell.withdraw(150)
 #print a statement
 mitchell.print_statement()
+
+#stretch challenge - checking vs savings interest
+bobs_bank_account = BankAccount ('Bob Bobson', ' 97811764', 'checking', 2500)
+bills_bank_account = BankAccount('Bill Billson', '335718401', 'savings', 2500)
+
+bobs_bank_account.add_interest()
+bobs_bank_account.print_statement()
+bills_bank_account.add_interest()
+bills_bank_account.print_statement()
