f8f88e1f3c0d456c045b0c3dc5595ac1bef82edb
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-24 22:59:37 -0500

    cleaned up code & added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bank of OOP
+
+## Description
+
+This python script allows us to run all the basic functions of an ATM, including account creation, deposits, withdraws, balance checks, and even interest accrual. 
+
+Each bank account include 4 attributes:
+* full name 
+* account number
+* account type
+* current balance
+
+At this point the functions are all run through the code in the script, but future iterations could include a terminal interface that accepts user prompts to run commands. 
--- script.py
+++ script.py
@@ -50,37 +50,33 @@ class BankAccount:
 print('---------------- Program Test Code ----------------')
 print('Test class instance and methods')
 print('----------------------------------------------------')
+# create an account and test each of the methods
 brooklyn = BankAccount('Brooklyn', '227658021', 'savings', 1000.00)
 brooklyn.deposit(120)
 brooklyn.withdraw(1000)
 brooklyn.get_balance()
 brooklyn.add_interest()
 brooklyn.print_statement()
-brooklyn.deposit(341.33)
 
 ### Sample Code
 print('-------------------- Sample Code --------------------')
 print ('Create a bank account for Mitchell')
 print('-----------------------------------------------------')
+
 mitchell = BankAccount('Mitchell Hudson', '3141592', 'checking', 0)
-#deposit $400,000 into "Mitchell's" account. 
-mitchell.deposit(400000)
-#print a statement
-mitchell.print_statement()
-#add interest to the account
-mitchell.add_interest()
-#print a statement
-mitchell.print_statement()
-#make a withdraw of $150 for yeezy's 
-mitchell.withdraw(150)
-#print a statement
-mitchell.print_statement()
+mitchell.deposit(400000) #deposit $400,000 into "Mitchell's" account. 
+mitchell.print_statement() 
+mitchell.add_interest() #add interest to the account
+mitchell.print_statement() 
+mitchell.withdraw(150) #make a withdraw of $150 for yeezy's 
+mitchell.print_statement() 
 
 # stretch challenge 1 - 
 # #checking vs savings interest
 print('---------------- Stretch Challenge 1 ----------------')
 print('See different interest accrual between savings vs. checking accounts')
 print('-----------------------------------------------------')
+
 bobs_bank_account = BankAccount ('Bob Bobson', ' 97811764', 'checking', 2500)
 bills_bank_account = BankAccount('Bill Billson', '335718401', 'savings', 2500)
 
